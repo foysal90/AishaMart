91fb93312f5cae3344cc454d8618434b761f21a0
foysal90 <foysal1416@hotmail.com>
2024-02-16 17:37:18 -0500

    search and pagination imolemented

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,9 +1,44 @@
 const createError = require("http-errors");
+const User = require("../models/userModel");
 
-
-const getUsers = (req, res, next) => {
+const getUsers = async (req, res, next) => {
   try {
-    res.status(200).send();
+    const search = req.query.search || "";
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 3;
+
+    //using reg exp for seach
+    const searchRegExp = new RegExp(search, "i"); // 'i' for case-insensitive
+
+    const filter = {
+      isAdmin: { $ne: true },
+      $or: [
+        { name: { $regex: searchRegExp } },
+        { email: { $regex: searchRegExp } },
+        { phone: { $regex: searchRegExp } },
+      ],
+    };
+    const options = { password: 0 };
+    const users = await User.find(filter, options)
+      //pagination implementing
+      .limit(limit)
+      .skip((page - 1) * page);
+    //counting total number of pages to display in front end
+
+    const count = await User.find(filter).countDocuments();
+    if (!users) {
+      throw createError(404, "no users were found");
+    }
+    res.status(200).send({
+      users,
+      //pagination for front end
+      pagination: {
+        totalPage: Math.ceil(count / limit),
+        currentPage: page,
+        previousPage: page - 1 > 0 ? page - 1 : null,
+        nextPage: page + 1 <= Math.ceil(count / limit) ? page + 1 : null,
+      },
+    });
   } catch (error) {
     next(error);
   }
--- backend/src/data.js
+++ backend/src/data.js
@@ -1,22 +1,78 @@
 const data = {
   users: [
     {
+      name: "Jane Smith",
+      email: "jane.smith@example.com",
+      password: "simplepassword",
+      phone: "098-765-4321",
+      address: "orlando, fl",
+    },
+
+    {
+      name: "Bob Brown",
+      email: "bob.brown@example.com",
+      password: "bobbrown321",
+      phone: "321-654-9870",
+      address: "orlando, fl",
+    },
+    {
+      name: "Charlie Davis",
+      email: "charlie.davis@example.com",
+      password: "charliepassword",
+      phone: "789-012-3456",
+      address: "orlando, fl",
+    },
+    {
+      name: "Diana Evans",
+      email: "diana.evans@example.com",
+      password: "diana1234",
+      phone: "654-321-0987",
+      address: "orlando, fl",
+    },
+    {
+      name: "Ethan Ford",
+      email: "ethan.ford@example.com",
+      password: "ethanford456",
+      phone: "567-890-1234",
+      address: "orlando, fl",
+    },
+    {
+      name: "Fiona Green",
+      email: "fiona.green@example.com",
+      password: "fionagreen789",
+      phone: "234-567-8910",
+      address: "orlando, fl",
+    },
+    {
+      name: "George Hill",
+      email: "george.hill@example.com",
+      password: "georgepassword",
+      phone: "890-123-4567",
+      address: "orlando, fl",
+    },
+    {
+      name: "Hannah Ives",
+      email: "hannah.ives@example.com",
+      password: "hannahives123",
+      phone: "123-890-4567",
+      address: "orlando, fl",
+    },
+
+    {
       name: "John Doe",
       email: "john.doe@example.com",
       password: "1223456",
-      phone: '123654789',
+      phone: "123654789",
       address: "orlando, fl",
-      
     },
-    
+
     {
       name: "Alice Johnson",
       email: "alice.johnson@example.com",
       password: "1223456",
-      phone: '123654789',
+      phone: "123654789",
       address: "orlando, fl",
     },
-  
   ],
 };
 module.exports = data;
